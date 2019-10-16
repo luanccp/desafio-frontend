b5cbb7ff0a60571ab2645bdadaab43233b70ed8c
Luan Castheloge <luanccp@gmail.com>
2019-10-16 13:56:47 -0300

    [REFACT] ident and refact some lines

--- src/pages/repository.js
+++ src/pages/repository.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 import api from "../services/api"
 import BranchCard from "../components/BranchCard";
@@ -28,6 +27,7 @@ const RepositoryPage = ({ location }) => {
 
     fetch()
   }, [])
+  
   return (
     <Layout>
       <h1>{(((location || {}).state || {}).repo || {}).name}</h1>
